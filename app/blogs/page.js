f0a80cef0f8310eb552fdfaa08e8f14c5ae07409
Abhi Sharma <me.avish143@gmail.com>
2024-12-13 23:14:58 +0530

    Blogs section done

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -3,8 +3,18 @@ import BlogCard from "../components/BlogCard";
 
 const index = () => {
   return (
-    <div>
-      <BlogCard />
+    <div className="container mx-auto px-4 max-w-7xl">
+      {/* Heading */}
+      <h1 className="font-bold text-center text-2xl md:text-3xl ">Stay Updated With Our Blogs</h1>
+      {/* Bottom Container with blogs. */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-8">
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+      </div>
     </div>
   );
 };
--- app/components/LatestBlogs.js
+++ app/components/LatestBlogs.js
@@ -3,20 +3,19 @@ import BlogCard from "./BlogCard";
 const LatestBlogs = () => {
   return(
     // Main Container
-      <div className="flex flex-col items-center gap-3 ">  
+      <div className="flex flex-col items-center gap-3 p-4">  
       {/* Heading  */}
-        <h1 className="font-bold text-[30px] text-center">Latest Blogs</h1>
+        <h1 className="font-bold text-[30px] text-center mb-4 sm:mb-6 lg:mb-8">Latest Blogs</h1>
         {/* Grid for blogs */}
-        <div className="grid grid-cols-1  gap-4  md:grid-cols-2 lg:grid-cols-3 items-center justify-center">
+        <div className="grid grid-cols-1 gap-6 sm:gap-8 lg:gap-10 md:grid-cols-2 lg:grid-cols-3 items-center justify-center w-full max-w-7xl">
           <BlogCard/>
           <BlogCard/>
           <BlogCard/>
         </div>
 
-        <button className="mt-4 relative border-b-8 px-7 py-3 w-[300px] shadow-xl text-center text-greenTitle font-semibold rounded-full border-2 border-black ">
-  View All Blogs
-</button>
-
+        <button className="mt-8 sm:mt-10 lg:mt-12 relative border-b-8 px-7 py-3 w-[300px] shadow-xl text-center text-greenTitle font-semibold rounded-full border-2 border-black ">
+          View All Blogs
+        </button>
       </div>
   )
 };
