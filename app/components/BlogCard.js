3192c021def2d2884bd970ac5098c74fc0d1b80e
Atif <atif276ali@gmail.com>
2025-01-19 20:42:02 +0530

    update: blog fix

--- app/components/BlogCard.js
+++ app/components/BlogCard.js
@@ -1,20 +1,28 @@
+import Image from "next/image";
 const BlogCard = () => {
   return (
     <div className="flex cursor-pointer flex-col gap-4 border-2 border-b-8 border-black rounded-lg rounded-b-3xl rounded-t-3xl shadow-lg w-full max-w-[390px] min-h-[400px]">
       {/* img */}
-      <img 
-        className="w-full h-48 object-cover rounded-lg rounded-t-3xl  " 
-        src="https://s3-alpha-sig.figma.com/img/1bbf/3a53/303668a9bfd0503050e1c33e0106e708?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aEOyvNWY9kLgyeHt4qKXdzt0Lk0Sch4dd-j8NXI9q4F~4nspJgWTYKb9tAjEt1WeQq5T0NuqPiWN2jiHPaKdo8BHvtFj3XAFL01qU-oNwNcU65mO5hJ0sCQGOowx05A3amiJeKVYwVR6-uFw9I3cooA3DJBTE1a6K6lYytOODelg-9FRjPSCnTSHkoNA7EkxZWq1AlfeWPEeg0ZS6LluR8kRof3YREguMGyaOTw8DhEhOGvEczT7n0kkyikzy7DIk28tS1cmMZIbFP7ZonayfRyjDsFQdMzWreStSQ7n2BYO3w2~l1~YKdDrhT18-CV64PfOJlMFXemiEXP5d5pcbg__"
+      <Image
+        className="w-full h-48 object-cover rounded-lg rounded-t-3xl  "
+        src="https://s3-alpha-sig.figma.com/img/1bbf/3a53/303668a9bfd0503050e1c33e0106e708?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aEOyvNWY9kLgyeHt4qKXdzt0Lk0Sch4dd-j8NXI9q4F~4nspJgWTYKb9tAjEt1WeQq5T0NuqPiWN2jiHPaKdo8BHvtFj3XAFL01qU-oNwNcU65mO5hJ0sCQGOowx05A3amiJeKVYwVR6-uFw9I3cooA3DJBTE1a6K6lYytOODelg-9FRjPSCnTSHkoNA7EkxZWq1AlfeWPEeg0ZS6LluR8kRof3YREguMGyaOTw8DhEhOGvEczT7n0kkyikzy7DIk28tS1cmMZIbFP7ZonayfRyjDsFQdMzWreStSQ7n2BYO3w2~l1~YKdDrhT18-CV64PfOJlMFXemiEXP5d5pcbg"
         alt="Blog thumbnail"
+        width={100}
+        height={100}
       />
 
-    {/* Text In between */}
+      {/* Text In between */}
       <div className="w-full bg-white flex flex-col gap-2 p-4">
-        <p className="text-greenTitle text-xs sm:text-sm">Oct 17, 2024 • 5 Min Read</p>
-        <h1 className="font-bold text-xl sm:text-2xl">AI in product design: what to expect beyond the hype</h1>
-        <p className="font-thin text-gray-700 text-xs sm:text-sm">AI in product design: what to expect beyond the hype</p>
+        <p className="text-greenTitle text-xs sm:text-sm">
+          Oct 17, 2024 • 5 Min Read
+        </p>
+        <h1 className="font-bold text-xl sm:text-2xl">
+          AI in product design: what to expect beyond the hype
+        </h1>
+        <p className="font-thin text-gray-700 text-xs sm:text-sm">
+          AI in product design: what to expect beyond the hype
+        </p>
       </div>
-
 
       {/* Tags */}
       <div className="flex rounded-b-3xl flex-wrap items-center justify-between bg-white p-4 rounded-lg shadow-md">
@@ -32,7 +40,7 @@ const BlogCard = () => {
         </button>
       </div>
     </div>
-  )
+  );
 };
 
 export default BlogCard;
